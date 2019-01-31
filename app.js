2e0e2770d6f3051ab1fac55bb300bd862ae181b8
Mark <markherrington5@gmail.com>
2019-01-31 11:44:53 -0500

    faded out welcome, worked on inputbox

---
+++ app.js
@@ -0,0 +1,7 @@
+// input box character counter
+$(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
+
+// fades out welcome message
+$("#welcomeText").delay(2000).fadeOut(2000);
