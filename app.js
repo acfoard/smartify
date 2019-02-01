f95f69362c75b831a04fa49f2f4787f7d6c54a5f
Timothy Cook <tim.wade.cook@gmail.com>
2019-02-01 13:36:57 -0500

    all three files up to date, app.js, index.html, main.css are now latest version of marks and mine. the side bar drawer pops out

--- app.js
+++ app.js
@@ -3,5 +3,57 @@ $(document).ready(function() {
     $('input#input_text, textarea#textarea2').characterCounter();
   });
 
+// sidenav
+$(document).ready(function(){
+ $('.sidenav').sidenav();
+});
+
+// modal
+$(document).ready(function(){
+ $('.modal').modal();
+});
+
 // fades out welcome message
 // $("#welcomeText").delay(2000).fadeOut(2000);
+
+
+// onSubmit => runInput(string) take in and save input message from the input form
+const runInput = function(e) {
+  e.preventDefault;
+// validate string
+  switch ( inputString ) {
+    case '': 
+      alert('Please enter text before hitting submit');
+      break;
+    case inputString.length > 260: 
+      alert('Please limit your input text to 260 characters')
+      break;
+    default:
+
+
+
+  }
+
+// save val() and trim();
+
+// renderInput() on page as a chat bubble sitting at bottom of page 
+
+// renderTypingIndicator()
+
+// call getResponse()
+}
+
+
+
+
+// call renderResponse
+
+
+// call 
+
+
+
+
+
+// Add event listener to form to run runInput() on 'click' or 'enter'
+$('#inputForm').on('submit', runInput);
