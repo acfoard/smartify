b715bd072862b3a1d209c91bf8f091562599c61e
Mark <markherrington5@gmail.com>
2019-02-04 12:47:00 -0500

    added favicon

--- app.js
+++ app.js
@@ -1,46 +1,42 @@
 // input box character counter
-$(document).ready(function() {
-    $('input#input_text, textarea#textarea2').characterCounter();
-  });
+$(document).ready(function () {
+  $('input#input_text, textarea#textarea2').characterCounter();
+});
 
 // sidenav
-$(document).ready(function(){
- $('.sidenav').sidenav();
+$(document).ready(function () {
+  $('.sidenav').sidenav();
 });
 
 // modal
-$(document).ready(function(){
- $('.modal').modal();
+$(document).ready(function () {
+  $('.modal').modal();
 });
 
 // fades out welcome message
 // $("#welcomeText").delay(2000).fadeOut(2000);
 
-
 // onSubmit => runInput(string) take in and save input message from the input form
-const runInput = function(e) {
+const runInput = function (e) {
   e.preventDefault;
-// validate string
-  switch ( inputString ) {
-    case '': 
+  // validate string
+  switch (inputString) {
+    case '':
       alert('Please enter text before hitting submit');
       break;
-    case inputString.length > 260: 
+    case inputString.length > 260:
       alert('Please limit your input text to 260 characters')
       break;
     default:
-
-
-
   }
 
-// save val() and trim();
+  // save val() and trim();
 
-// renderInput() on page as a chat bubble sitting at bottom of page 
+  // renderInput() on page as a chat bubble sitting at bottom of page 
 
-// renderTypingIndicator()
+  // renderTypingIndicator()
 
-// call getResponse()
+  // call getResponse()
 }
 
 
